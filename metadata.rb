cb969e998df9c4ee16d4704581739545a6ce8985
Michael Lewis <frightenedmonkey@gmail.com>
2017-10-19 17:48:06 -0700

    Add some environment variables & CLI config to unit file

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'hja22@hja22.co.uk'
 license 'MIT'
 description 'Installs/Configures Jupyter and runs notebook as a service'
 long_description 'Installs/Configures Jupyter and creates, enables and starts a service to run a Jupyter notebook server'
-version '0.1.1'
+version '0.1.3'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 supports 'ubuntu', '= 16.04'
